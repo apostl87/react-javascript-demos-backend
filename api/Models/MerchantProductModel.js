59e8843157124c1587f20a7fa693709f37796e98
Andreas Postl <42180854+apostl87@users.noreply.github.com>
2024-07-08 01:15:21 +0200

    Comment out log commands for debugging

--- api/Models/MerchantProductModel.js
+++ api/Models/MerchantProductModel.js
@@ -28,12 +28,11 @@ const updateMerchantProduct = (mp_merchant_user_id, mp_id, body) => {
                 WHERE mp_merchant_user_id = '${mp_merchant_user_id}' AND mp_id = '${mp_id}'
                 RETURNING *`;
 
-        console.log(psql);
-
         pool.query(
             psql,
             (error, results) => {
                 if (error) {
+                    console.log(error);
                     reject(error);
                 }
                 // console.log(results.rows)
@@ -53,7 +52,7 @@ const createMerchantProduct = (body) => {
         let psql = `INSERT INTO merchant_products (mp_name, mp_c_id_production, mp_color, mp_weight_kg, mp_price, mp_currency, mp_image_url, mp_merchant_user_id)
                 VALUES ('${mp_name}', ${mp_c_id_production}, '${mp_color}', '${mp_weight_kg}', '${mp_price}', '${mp_currency}', '${mp_image_url}', '${mp_merchant_user_id}') RETURNING *;`;
 
-        console.log(psql)
+        //console.log(psql)
 
         pool.query(
             psql, (error, results) => {
@@ -76,7 +75,8 @@ const initWithTestData = (mp_merchant_user_id) => {
     let psql = require('../data_templates/init_merchant_products.js');
     psql = psql.replace(/{mp_merchant_user_id}/g, mp_merchant_user_id);
     psql += psql_select_all(mp_merchant_user_id)
-    console.log(psql);
+    
+    // console.log(psql);
 
     return new Promise(function (resolve, reject) {
         pool.query(psql, (error, results) => {
